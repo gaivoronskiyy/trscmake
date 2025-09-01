8b64eebc78b82f8d319dc90d571318ada3f8e4d7
gaivoronskiyy <perseuspers9@gmail.com>
2025-09-01 21:10:47 +0300

    source 1
    source for compilation

---
+++ trsys_CMake.cpp
@@ -0,0 +1,325 @@
+﻿// trsys_CMake.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <fstream>
+#include "trsys_CMake.h"
+
+#pragma comment(linker, "/STACK:2000000")
+#pragma comment(linker, "/HEAP:2000000")
+
+using namespace std;
+
+
+
+void init_screen(void)
+{
+	glMatrixMode(GL_PROJECTION);
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+void reshape_saddle21(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-5.0, 7.1, -9.0, 10.1);
+
+
+	float lx = 0.17f, lz = 0.0f;
+	float x = -5.4f, z = 2.4f;
+	gluLookAt(x, 1.4f, z,
+		x + lx, 0.1f, z + lz,
+		0.2f, 1.2f, -0.05f);
+
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void reshape_saddle3(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 12, 0, 12);
+	glTranslatef(-1.0, 0.0, -3.0);
+	glRotatef(-30, 0, 0, 1);
+	glRotatef(-30, 1, 0, 0);
+	glRotatef(10, 0, 1, 0);
+	
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void reshape_saddle7(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-5, 7, -9, 10);
+	glTranslatef(7.0, 5.0, 5.0);
+	glRotatef(-31, 0, 0, 1);
+	glRotatef(-31, 1, 0, 0);
+	glRotatef(201, 0, 1, 0);
+
+
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+boost::scoped_ptr <triangle_service> t(new triangle_service());
+
+
+int main(int argc, char *argv[])
+{
+
+	int i = 0, j = 0;// , i1 = 0, i2 = 1, *i2_p = nullptr;
+	//i2_p = &i2;
+	float sc = 0;
+	char *buff = new char[1000];
+
+	ifstream inn;
+
+	linear_calculation *l = new linear_calculation();
+
+
+	while (j < argc) {
+		switch (j) {
+		case 0:
+			if (argc != 6) {
+				cout << "Invalid parametres!!!" << endl;
+				return 1;
+			}
+			break;
+		case 1:
+			if (strlen(argv[j]) != 2 || strstr(argv[j], "-i") == NULL) {
+				cout << "Invalid key parametres!!!" << endl;
+				return 1;
+			}
+			break;
+		case 2:
+			inn.open(argv[j]);
+			if (!inn) {
+				cout << "Can't read file!!!" << endl;
+				return 1;
+			}
+			break;
+		case 3:
+			if (strlen(argv[j]) != 2 || strstr(argv[j], "-o") == NULL) {
+				cout << "Invalid key parametres!!!" << endl;
+				return 1;
+			}
+			break;
+
+		}
+
+		j++;
+	}
+	j = 0;
+
+	
+	while (inn.getline(buff, 1000)) {
+
+		t->pars(buff);
+		t->coo.i = i;
+		if (i > 0) {
+			sc = t->scalar_prod(t->coo);
+			t->coord_m.emplace(sc, t->coo);
+			t->coord_m0.emplace(i, t->coo);
+		}
+		i++;
+
+		memset(buff, 0, strlen(buff));
+	}
+
+
+	t->space_scan(t->coord_mp0, t->coord_m);
+	l->pakgage_func(t->max_i, t->max_j, t->triangle_p, t->triangl_tr);
+	
+	
+	ofstream outt, outt0;
+
+	outt.open("C:\\Users\\c\\source\\repos\\p1.txt", ios::app);
+
+	//for the formatting out 
+
+	auto ex_arr = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[0];
+			try {
+				v[0].at(0);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[0].at(0);
+	};
+
+	auto ex_arr1 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[0];
+			try {
+				v[0].at(1);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[0].at(1);
+	};
+
+	auto ex_arr00 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[1];
+			try {
+				v[1].at(0);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[1].at(0);
+	};
+	auto ex_arr01 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[1];
+			try {
+				v[1].at(1);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[1].at(1);
+	};
+	auto ex_arr10 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[2];
+			try {
+				v[2].at(0);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[2].at(0);
+	};
+	auto ex_arr11 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[2];
+			try {
+				v[2].at(1);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[2].at(1);
+	};
+
+	auto ex_arr20 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[3];
+			try {
+				v[3].at(0);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[3].at(0);
+	};
+	auto ex_arr21 = [](boost::container::small_vector<boost::array<int, 2>, 3> v) {
+
+		try {
+			v[3];
+			try {
+				v[3].at(1);
+			}
+			catch (out_of_range &e) {
+				cout << e.what() << endl;
+				return 0;
+			}
+		}
+		catch (out_of_range &e) {
+			cout << e.what() << endl;
+			return 0;
+		}
+
+		return v[3].at(1);
+	};
+
+	
+	for (auto &m1 : t->triangle) { // print nodes
+		cout << m1.first << "  " << m1.second.first.node1 << "  " << m1.second.first.node2 << "  " << m1.second.first.node3 << " arr_dead_edg: " << ex_arr(m1.second.second) << " " << ex_arr1(m1.second.second) << "   " << ex_arr00(m1.second.second) << " " << ex_arr01(m1.second.second) << "   " << ex_arr10(m1.second.second) << " " << ex_arr11(m1.second.second) << endl;
+		outt << m1.first << "," << m1.second.first.node1 << "," << m1.second.first.node2 << "," << m1.second.first.node3 << " arr_dead_edg: " << ex_arr(m1.second.second) << " " << ex_arr1(m1.second.second) << "   " << ex_arr00(m1.second.second) << " " << ex_arr01(m1.second.second) << "   " << ex_arr10(m1.second.second) << " " << ex_arr11(m1.second.second) << endl;
+	}
+
+	outt.close();
+
+	
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);                    // window size
+	glutInitWindowPosition(500, 500);                // distance from the top-left screen
+	init_screen();
+	glutCreateWindow("trsystems");    // message displayed on top bar window
+	glutDisplayFunc([]() {t->displayMe(); });
+	glutReshapeFunc(reshape_saddle21);
+	glutMainLoop();
+	
+	//system("pause");
+}
---
+++ trsys_CMake.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include <math.h>
+#include <chrono>
+#include <thread>
+#include <boost/optional.hpp>
+#include <boost/rational.hpp>
+#include <boost/math/special_functions.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/move/unique_ptr.hpp>
+#include <boost/array.hpp>
+#include <boost/container/map.hpp>
+#include <boost/container/small_vector.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/function.hpp>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+
+
+class triangle_service {
+public:
+	triangle_service() : coo_p(&coo), nodes_p(&nodes), triangle_p(&triangle), triangle_tw0(&triangle_tw), tr_plan_p(&tr_plan), d_key(&dead_key), d_key1(&dead_key1), d_point(&dead_point), coord_mp0(&coord_m0), i1_p(&i1), i2_p(&i2){};
+	virtual ~triangle_service() {};
+	
+	struct coord {
+		int i;
+		float x;
+		float y;
+		float z;
+	} coo_s{ 0,0,0,0 }, coo{ 0,0,0,0 }, *coo_p;
+	
+
+	struct tr_node {
+		int node1;
+		int node2;
+		int node3;
+	} nodes{0,0,0}, *nodes_p;
+
+	int pars(char *buff);
+	float scalar_prod(coord c);
+	bool get_free_nodes(boost::container::small_vector<int*, 3> a, boost::container::small_vector<boost::array<int, 2>, 3> b, bool &nod1, bool &nod2, bool &nod3);
+	void get_free_nodes0(boost::container::small_vector<boost::array<int, 2>, 3> a, boost::container::small_vector<boost::array<int, 2>, 3> b, bool &nod1, bool &nod2, bool &nod3);
+	bool get_dead_nodes(boost::container::small_vector<boost::array<int, 2>, 3> *a_p, boost::container::small_vector<boost::array<int, 2>, 3> a, boost::container::small_vector<boost::array<int, 2>, 3> b, int f);
+    int space_scan(boost::container::map<int, coord> *coord_mp, boost::container::map<float, coord> &coord_m);
+	size_t hash_umap(std::pair<float, float>);
+	void displayMe();
+
+	boost::unordered_set<int> dead_key, *d_key, dead_key1, *d_key1, dead_point, *d_point;
+	boost::container::map<float, coord> coord_m;
+	boost::container::map<int, coord> coord_m0, *coord_mp0;
+	typedef boost::function<size_t(std::pair<float, float>)> has;
+	boost::container::map<int, coord>::iterator it_coord;
+	boost::container::small_vector<boost::array<int, 2>, 3> edg{ {0,0},{0,0},{0,0} };
+	boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> triangle, triangle_tw, *triangle_p, *triangle_tw0;
+	boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> triangl_tr;
+	boost::container::map<int, std::pair<tr_node, boost::array<float, 4>>> tr_plan, *tr_plan_p;
+	std::pair<float, float> key;
+	int max_i = 0, max_j = 0;
+	int tmp_i0 = 0, tmp_i = 0, i1 = 0, i2 = 1, *i1_p = nullptr, *i2_p = nullptr;
+	
+};
+
+
+class linear_calculation : public triangle_service {
+public:
+	linear_calculation(){};
+	~linear_calculation() {};
+
+
+	bool create_triangle(int point, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> &triangl_tr);
+	bool create_triangle_closed(boost::container::map<int, coord>::iterator it_coord, boost::container::map<int, coord> coord_m0, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> triangle, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_tw0, tr_node *nodes_p, int &i2, int *i2_p, boost::unordered_set<int> *d_key, boost::unordered_set<int> *d_key1, boost::container::map<int, std::pair<tr_node, boost::array<float, 4>>> *tr_plan_p, boost::container::map<float, coord> coord_m);
+	void plan(int *i2_p, tr_node *nodes_p, boost::container::map<int, coord> coord, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::map<int, std::pair<tr_node, boost::array<float, 4>>> *tr_pl);
+	int intersection_triangle(double key_tmp_tr, int point, int flag);
+	bool linker_tr(int point, int point1, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> &triangl_tr);
+	bool dis11(std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>> a, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>> b, boost::container::map<int, coord> coord_m0, boost::array<int, 3> *tr0_p, boost::container::small_vector<boost::array<int, 2>, 3> *edge_tr1_p, bool inter);
+	bool alone_plan(int t_n0, int t_n1, boost::container::map<int, std::pair<tr_node, boost::array<float, 4>>> *tr_plan_p);
+	bool edge_check(boost::container::small_vector<boost::array<int, 2>, 3> a, boost::container::small_vector<boost::array<int, 2>, 3> b, int l);
+	void weight(int mls);
+	bool tr_alg(int x, int y, int max_i, int max_j, int t_i, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> &triangl_tr);
+	bool tr_link_alg(int p, int x, int y, int max_i, int max_j, int i0, int j0, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> &triangl_tr);
+	void pakgage_func(int max_i, int max_j, boost::container::map<int, std::pair<tr_node, boost::container::small_vector<boost::array<int, 2>, 3>>> *triangle_p, boost::container::multimap<double, std::pair<tr_node, std::pair<int, boost::array<int, 2>>>> &triangl_tr);
+	void map_s_print(int max_i, int max_j);
+
+
+};
